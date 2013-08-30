8b11450fa7a277384e5366c3319e5314cd3a38a1
TANABE Ken-ichi <nabeken@tknetworks.org>
2013-08-30 09:53:59 +0900

    0.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "nabeken@tknetworks.org"
 license          "Apache 2.0"
 description      "Installs/Configures ssh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.3"
 
 supports 'ubuntu'
